e36a703971ef20bd9451fb9b8ed14c6f5cc57dc4
Naomi-Rhames <naomirhames2020@gmail.com>
2022-08-23 13:24:04 -0500

    Adding to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 class PostsController < ApplicationController
+ 
+    def create(post_params)
+            post = Post.new(post_params)
+            if post.save
+                render json: post
+            else
+                render json: {error: post.errors.full_messages},  status: 403
+            end
+        end
+    
+        private
+        
+        def post_params
+            params.require(:post).permit(:message, :user_id )
+        end
 
    
 end
